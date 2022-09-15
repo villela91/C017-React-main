fdfd915abf9f3e95b501127dc0531321ae5fc369
LUIZ VILLELA <106358282+villela91@users.noreply.github.com>
2022-09-15 10:33:11 -0300

    https://github.com/villela91/C017-React-main

--- client/src/pages/home/home.js
+++ client/src/pages/home/home.js
@@ -1,13 +1,42 @@
 import { Card } from "../../components/card/card";
+import { Header } from "../../header/header";
 import { api } from "../../utils/api/api";
 import { useState, useEffect } from "react";
+import { CgClose } from "react-icons/cg";
+import Modal from "react-modal";
 import "./home.css";
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    width: "40rem",
+    height: "50rem",
+    transform: "translate(-50%, -50%)",
+    backgroundColor: " rgba(0, 0, 0, 0.8)",
+    borderRadius: "15px",
+  },
+  overlay: {
+    background: "rgba(0, 0, 0, 0.4)",
+  },
+};
+
+Modal.setAppElement("#root");
 export function Home() {
   const [animeList, setAnimeList] = useState([]);
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [uniqueAnime, setUniqueAnime] = useState({});
 
   async function getAnime() {
     const animes = await api.getAllAnimes();
     setAnimeList(animes);
+  }
+
+  function handleModal() {
+    setModalIsOpen(!modalIsOpen);
   }
 
   console.log(animeList);
@@ -30,19 +59,60 @@ export function Home() {
   console.log("Renderizou");
   return (
     <>
+      <Header />
       <div className="card-list">
         {animeList.map((item, index) => {
           return (
-            <Card
+            <button
+              className="button-card"
+              onClick={() => {
+                setUniqueAnime(item);
+                handleModal();
+              }}
               key={index}
-              title={item.title}
-              gender={item.gender}
-              protagonist={item.protagonist}
-              year={item.year}
-            />
+            >
+              <Card
+                title={item.title}
+                gender={item.gender}
+                protagonist={item.protagonist}
+                year={item.year}
+              />
+            </button>
           );
         })}
       </div>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={handleModal}
+        style={customStyles}
+        contentLabel="Card details"
+      >
+        <section>
+          <section
+            style={{
+              display: "flex",
+              width: "100%",
+              justifyContent: "flex-end",
+              alignItems: "center",
+            }}
+          >
+            <button
+              style={{
+                backgroundColor: "transparent",
+                cursor: "pointer",
+                border: "none",
+              }}
+              onClick={handleModal}
+            >
+              <CgClose size={28} color="red" />
+            </button>
+          </section>
+          <h2>{uniqueAnime.title}</h2>
+          <h3>{uniqueAnime.gender}</h3>
+          <h3>{uniqueAnime.protagonist}</h3>
+          <h3>{uniqueAnime.year}</h3>
+        </section>
+      </Modal>
     </>
   );
 }
